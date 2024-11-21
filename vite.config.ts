cb3e4d725727b8c5ebd2614092441b025565bf8a
NaclaraSantos <santoosclariinha@gmail.com>
2024-11-21 19:58:50 -0300

    Enviando alteração

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/acessomap/', // Isso é necessário para o GitHub Pages
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Diretório de saída de build
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
+
